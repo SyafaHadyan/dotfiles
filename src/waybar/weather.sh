67ebed6846e0161ae0819d9d5d8510e7958bc666
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2025-03-30 08:10:48 +0700

    edit source env file

--- src/waybar/weather.sh
+++ src/waybar/weather.sh
@@ -3,5 +3,6 @@
 location=$(curl -s "https://ipinfo.io" | jq ".loc")
 latitude=$(echo $location | cut -d "," -f 1 | sed s/[\"]//g)
 longitude=$(echo $location | cut -d "," -f 2 | sed s/[\"]//g)
-source .env
-curl -s "https://api.openweathermap.org/data/3.0/onecall?lat=$latitude&lon=$longitude&units=metric&exclude=minutely,hourly,daily,alerts&appid=$OPENWEATHER_API" | jq ".current.temp"
+source ~/.config/waybar/.env
+weather=$(curl -s "https://api.openweathermap.org/data/3.0/onecall?lat=$latitude&lon=$longitude&units=metric&exclude=minutely,hourly,daily,alerts&appid=$OPENWEATHER_API" | jq ".current.temp")
+echo $weather"°C"
